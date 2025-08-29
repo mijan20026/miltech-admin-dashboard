99937a8aba91c44c838182c10367d0ef3d73e9b0
Mijanr Rahman <mijan17634@gmail.com>
2025-08-29 12:41:42 +0600

    Sidebar fixed

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -4,33 +4,19 @@ import Header from "./Header";
 import { Outlet } from "react-router-dom";
 
 const Main = () => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsCollapsed((prev) => !prev);
-  };
+  const [collapsed, setCollapsed] = useState(false);
 
   return (
     <div className="h-screen w-screen flex bg-baseBg overflow-hidden">
       {/* Sidebar */}
-      <div
-        className={`${
-          isCollapsed ? "w-20" : "w-80"
-        } md:w-80 h-screen border-r-[1px] border-[#3FAE6A] transition-all duration-300`}
-      >
-        <Sidebar />
-      </div>
+      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
 
       {/* Main Content */}
-      <div className="flex flex-col flex-1 h-screen">
-        <Header toggleSidebar={toggleSidebar} />
-        <div className="bg-[#F6F6F6] h-[calc(100vh-68px)] mt-3">
-          {/* -68px */}
+      <div className="flex-1 flex flex-col h-screen transition-all duration-300">
+        <Header toggleSidebar={() => setCollapsed(!collapsed)} />
+        <div className="flex-1 mt-3 overflow-hidden">
           <div className="h-full overflow-y-auto bg-baseBg rounded-md p-7 pt-0">
-            {/* Outlet for other dynamic pages */}
-            <div className="mt-6">
-              <Outlet />
-            </div>
+            <Outlet />
           </div>
         </div>
       </div>
